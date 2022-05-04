15d5be80e916c6b10ee09551c4fbc1191053ab79
Terenty Rezman <terenty.rezman@mail.ru>
2022-05-04 23:01:26 +0300

    create binary tree from bfs array print binary tree as bfs

--- src/binary_tree.rs
+++ src/binary_tree.rs
@@ -20,18 +20,54 @@ impl TreeNode {
   }
 }
 
-pub fn from_bfs_array(array: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
-    let mut root: Option<Rc<RefCell<TreeNode>>> = None;
+pub fn from_bfs_array(array: &[Option<i32>]) -> Option<Rc<RefCell<TreeNode>>> {
     let mut values = VecDeque::from(array.to_vec());
-    let mut queue_to_process = VecDeque::from([&mut root]);
+    let mut root: Option<Rc<RefCell<TreeNode>>> = None;
 
-    while !queue_to_process.is_empty() && !values.is_empty() {
-        let node = queue_to_process.pop_front().unwrap();
-        let val = values.pop_front().unwrap();
+    if !array.is_empty() {
+      let mut to_process = VecDeque::new();
+      let val = values.pop_front().unwrap().unwrap();
 
-        let new_node = Rc::new(RefCell::new(TreeNode::new(val)));
-        node.replace(new_node);
+      root.replace(Rc::new(RefCell::new(TreeNode::new(val))));
+      to_process.push_back(root.as_ref().unwrap().clone());
+      
+      while !values.is_empty() {
+        let node = to_process.pop_front().unwrap();
+
+        if let Some(val ) = values.pop_front().unwrap() {
+          node.borrow_mut().left.replace(Rc::new(RefCell::new(TreeNode::new(val))));
+          to_process.push_back(node.borrow().left.clone().unwrap());
+        }
+
+        if values.is_empty() {
+          break;
+        }
+
+        if let Some(val ) = values.pop_front().unwrap() {
+          node.borrow_mut().right.replace(Rc::new(RefCell::new(TreeNode::new(val))));
+          to_process.push_back(node.borrow().right.clone().unwrap());
+        }
+      }
     }
 
     root    
+}
+
+pub fn print_level_order(root: &Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<i32>> {
+  let mut result = vec![];
+  let mut to_process = VecDeque::new();
+  to_process.push_back(root.clone());
+
+  while !to_process.is_empty() {
+    let node = to_process.pop_front().unwrap();
+    if let Some(node) = node {
+      result.push(Some(node.borrow().val));
+      to_process.push_back(node.borrow().left.clone());
+      to_process.push_back(node.borrow().right.clone());
+    } else {
+      result.push(None);
+    }
+  }
+
+  result
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod binary_tree;
 
 fn main() {
     // excercism::test()
-    let root = binary_tree::from_bfs_array(&[1, 2, 3]);
-
-    // my_list::test();
+    let root = binary_tree::from_bfs_array(&[Some(1), None, Some(3)]);
+    dbg!(binary_tree::print_level_order(&root));
 }
