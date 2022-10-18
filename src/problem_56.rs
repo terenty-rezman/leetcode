58463f7a88144101ed3dcbb7b667ca84ad02f320
Terenty Rezman <terenty.rezman@mail.ru>
2022-10-18 23:04:16 +0300

    problem_56 done

--- src/problem_56.rs
+++ src/problem_56.rs
@@ -1,21 +1,23 @@
 struct Solution;
 
 impl Solution {
-    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         if intervals.is_empty() {
             return vec![];
         }
+
+        intervals.sort_by(|a, b| a[0].cmp(&b[0]));
 
         let mut prev_start = intervals[0][0];
         let mut prev_end = intervals[0][1];
         let mut res = vec![];
         
-        for i in intervals {
+        for i in intervals.into_iter().skip(1) {
             let cur_start = i[0];
             let cur_end = i[1];
             
             if cur_start <= prev_end {
-                prev_end = cur_end;
+                prev_end = std::cmp::max(prev_end, cur_end);
             } else {
                 res.push(vec![prev_start, prev_end]);
                 prev_start = cur_start;
